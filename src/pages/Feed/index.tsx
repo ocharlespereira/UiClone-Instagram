07cd65e53601b7380ee23f838f207303732f3fc0
Charles Pereira <charlesti.ptu@gmail.com>
2021-01-18 17:21:57 -0300

    Mobile - config api rest

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Text } from "react-native";
 
-import { Text } from 'react-native';
+import api from "../../services/api";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 const Feed: React.FC = () => {
+  const [feed, setFeed] = useState([]);
+
+  useEffect(() => {
+    // ?_expand=author&_limit=5&_page=1
+    api
+      .get("feed", {
+        params: {
+          _expand: "author",
+          _limit: 5,
+          _page: 1,
+        },
+      })
+      .then((res) => {
+        setFeed(res.data);
+      })
+      .catch((error) => {
+        console.log("error => ", error);
+      });
+  }, []);
+
   return (
     <Container>
       <Text>Feed</Text>
